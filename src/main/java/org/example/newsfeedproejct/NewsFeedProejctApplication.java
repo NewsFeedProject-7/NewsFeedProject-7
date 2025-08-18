6eeecc90b333724ef953d21a741506476a6b00fb
seongmin2223 <khyuno9@naver.com>
2025-08-18 11:43:27 +0900

    first commit

---
+++ src/main/java/org/example/newsfeedproejct/NewsFeedProejctApplication.java
@@ -0,0 +1,13 @@
+package org.example.newsfeedproejct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFeedProejctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsFeedProejctApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/newsfeedproejct/NewsFeedProejctApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.newsfeedproejct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsFeedProejctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
