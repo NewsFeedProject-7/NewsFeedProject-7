839e45a8993e14d76dc591b8f1ae44404304bbde
김영훈 <hunifive@gmail.com>
2025-08-19 12:02:51 +0900

    feat(user): 회원가입 기능 추가

--- src/main/java/org/example/newsfeedproejct/user/controller/UserController.java
+++ src/main/java/org/example/newsfeedproejct/user/controller/UserController.java
@@ -1,10 +1,33 @@
 package org.example.newsfeedproejct.user.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.newsfeedproejct.user.dto.UserSignUpDto;
+import org.example.newsfeedproejct.user.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signup(
+            @Valid @RequestBody UserSignUpDto.Request requestDto
+    ) {
+
+        userService.signup(
+                requestDto.getNickname(),
+                requestDto.getEmail(),
+                requestDto.getPassword(),
+                requestDto.getConfirmPassword()
+        );
+    }
 
 }
--- src/main/java/org/example/newsfeedproejct/user/dto/UserSignUpDto.java
+++ src/main/java/org/example/newsfeedproejct/user/dto/UserSignUpDto.java
@@ -1,6 +1,8 @@
 package org.example.newsfeedproejct.user.dto;
 
-import lombok.Builder;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 public class UserSignUpDto {
@@ -8,12 +10,18 @@ public class UserSignUpDto {
     @Getter
     public static class Request {
 
-    }
+        @NotBlank(message = "이메일은 필수값입니다.")
+        @Email(message = "이메일 형식이 아닙니다.")
+        private String email;
 
-    @Getter
-    @Builder
-    public static class Response {
+        @NotBlank(message = "비밀번호는 필수값입니다.")
+        @Size(min = 4, message = "비밀번호는 최소 4자 이상이어야 합니다.")
+        private String password;
 
+        private String confirmPassword;
+
+        @NotBlank(message = "닉네임은 필수값입니다.")
+        private String nickname;
     }
 
 }
