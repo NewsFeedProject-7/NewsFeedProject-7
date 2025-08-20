3455ade5365a39464f775e7f2e335b42ba519de1
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-08-20 11:28:30 +0900

    feat(board): 피드 수정 기능 추가

--- src/main/java/org/example/newsfeedproejct/board/controller/BoardController.java
+++ src/main/java/org/example/newsfeedproejct/board/controller/BoardController.java
@@ -1,9 +1,11 @@
 package org.example.newsfeedproejct.board.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.newsfeedproejct.board.dto.BoardCreateDto;
 import org.example.newsfeedproejct.board.dto.BoardSearchDetailDto;
 import org.example.newsfeedproejct.board.dto.BoardSearchDto;
+import org.example.newsfeedproejct.board.dto.BoardUpdateDto;
 import org.example.newsfeedproejct.board.service.BoardService;
 import org.example.newsfeedproejct.global.consts.Const;
 import org.springframework.data.domain.Page;
@@ -37,5 +39,14 @@ public class BoardController {
     @ResponseStatus(HttpStatus.OK)
     public BoardSearchDetailDto.Response findById(@PathVariable Long id) {
         return boardService.findById(id);
+    }
+
+    // 피드 수정
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public BoardUpdateDto.Response updateBoard(@PathVariable Long id,
+                                               @Valid @RequestBody BoardUpdateDto.Request requestDto,
+                                               @SessionAttribute(Const.LOGIN_USER) Long loginUserId) {
+        return boardService.updateBoard(id, loginUserId, requestDto.getSubject(), requestDto.getContent());
     }
 }
--- src/main/java/org/example/newsfeedproejct/board/service/BoardService.java
+++ src/main/java/org/example/newsfeedproejct/board/service/BoardService.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.example.newsfeedproejct.board.dto.BoardCreateDto;
 import org.example.newsfeedproejct.board.dto.BoardSearchDetailDto;
 import org.example.newsfeedproejct.board.dto.BoardSearchDto;
+import org.example.newsfeedproejct.board.dto.BoardUpdateDto;
 import org.example.newsfeedproejct.board.entity.Board;
 import org.example.newsfeedproejct.board.repository.BoardRepository;
 import org.example.newsfeedproejct.comment.dto.CommentSearchDetailDto;
 import org.example.newsfeedproejct.comment.repository.CommentRepository;
+import org.example.newsfeedproejct.global.exception.GlobalException;
+import org.example.newsfeedproejct.global.exception.errorcode.BoardErrorCode;
 import org.example.newsfeedproejct.user.entity.User;
 import org.example.newsfeedproejct.user.repository.UserRepository;
 import org.springframework.data.domain.Page;
@@ -52,5 +55,19 @@ public class BoardService {
                 .map(CommentSearchDetailDto.Response::from)
                 .toList();
         return BoardSearchDetailDto.Response.from(findBoard, comments);
+    }
+
+    // 피드 수정
+    public BoardUpdateDto.Response updateBoard(Long id, Long loginUserId, String subject, String content) {
+        if (subject == null && content == null) {
+            throw new GlobalException(BoardErrorCode.BOARD_VALIDATION_FAILED);
+        }
+        Board findBoard = boardRepository.findByIdOrElseThrow(id);
+
+        if (!findBoard.getUser().getId().equals(loginUserId)) {
+            throw new GlobalException(BoardErrorCode.BOARD_UNAUTHORIZED);
+        }
+        findBoard.updateBoard(subject, content);
+        return BoardUpdateDto.Response.from(findBoard);
     }
 }
