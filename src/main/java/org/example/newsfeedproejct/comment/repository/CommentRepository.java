d43f5db3df7927fbe6020d7efad4938bf7841065
joon448 <joonjdy448@gmail.com>
2025-08-20 09:29:21 +0900

    feat(comment): 댓글 조회 DTO 추가

--- src/main/java/org/example/newsfeedproejct/comment/repository/CommentRepository.java
+++ src/main/java/org/example/newsfeedproejct/comment/repository/CommentRepository.java
@@ -5,6 +5,7 @@ import org.example.newsfeedproejct.global.exception.GlobalException;
 import org.example.newsfeedproejct.global.exception.errorcode.CommentErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
@@ -12,4 +13,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     default Comment findByIdOrElseThrow(Long commentId){
         return findByIdAndDeletedAtIsNull(commentId).orElseThrow(() -> new GlobalException(CommentErrorCode.COMMENT_NOT_FOUND));
     }
+
+    List<Comment> findAllByBoardIdOrderByCreatedAt(Long boardId);
 }
