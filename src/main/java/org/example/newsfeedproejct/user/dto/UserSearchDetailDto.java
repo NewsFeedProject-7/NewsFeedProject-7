eac0bad228051f7ee039690129b0420de2cd66de
seongmin2223 <khyuno9@naver.com>
2025-08-20 10:44:06 +0900

    feat(user): 프로필 조회 기능 추가

--- src/main/java/org/example/newsfeedproejct/user/dto/UserSearchDetailDto.java
+++ src/main/java/org/example/newsfeedproejct/user/dto/UserSearchDetailDto.java
@@ -1,32 +1,22 @@
 package org.example.newsfeedproejct.user.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.example.newsfeedproejct.user.entity.User; // User 엔티티 import
-import org.example.newsfeedproejct.user.validation.annotation.ValidEmail;
-import org.example.newsfeedproejct.user.validation.annotation.ValidNickname;
+import org.example.newsfeedproejct.user.entity.User;
 
 public class UserSearchDetailDto {
-
     @Getter
-    @RequiredArgsConstructor
-    public static class Request {
-        @NotBlank(message = "아이디값은 필수값입니다.")
-        private final Long id;
-    }
-
-    @Getter
+    @Builder
     public static class Response {
-        @ValidEmail
         private final String email;
-
-        @ValidNickname
         private final String nickname;
 
-        public Response(User user) {
-            this.email = user.getEmail();
-            this.nickname = user.getNickname();
+        public static Response from(User user) {
+            return Response.builder()
+                    .email(user.getEmail())
+                    .nickname(user.getNickname())
+                    .build();
+
         }
     }
 }
