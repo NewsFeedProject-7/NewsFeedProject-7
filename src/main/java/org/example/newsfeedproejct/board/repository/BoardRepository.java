ba0514fd37e1a22a3ab9701d4a9c75d6dc3e0c4d
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-08-21 14:45:25 +0900

    fix(board): 단건 조회 EntityGraph 추가

--- src/main/java/org/example/newsfeedproejct/board/repository/BoardRepository.java
+++ src/main/java/org/example/newsfeedproejct/board/repository/BoardRepository.java
@@ -3,11 +3,13 @@ package org.example.newsfeedproejct.board.repository;
 import org.example.newsfeedproejct.board.entity.Board;
 import org.example.newsfeedproejct.global.exception.GlobalException;
 import org.example.newsfeedproejct.global.exception.errorcode.BoardErrorCode;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
+    @EntityGraph(attributePaths = {"user"})
     Optional<Board> findByIdAndDeletedAtIsNull(Long id);
 
     default Board findByIdOrElseThrow(Long id) {
